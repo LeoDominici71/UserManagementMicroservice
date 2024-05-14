355b6515a09773427c2aa91bf7983d8bc271d068
LeoDominici71 <leonardo_requena@outlook.com>
2024-05-14 13:24:15 -0300

    final commit

--- src/main/java/com/fiap/clientRegistry/ClientRegistryApplication.java
+++ src/main/java/com/fiap/clientRegistry/ClientRegistryApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@LoadBalancerClient(name = "orderManagement")
+@LoadBalancerClient(name = "userManagement")
 @EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class ClientRegistryApplication {
 
